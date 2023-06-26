670b2df8ba80ddb049fb8e2e41d1e6eee28dd247
BeastlyGabi <93128334+BeastlyGabi@users.noreply.github.com>
2023-06-26 13:28:27 -0400

    fix linux compilation need to replace libvlc with hxcodec

--- source/vlc/cpp/src/LibVLC.cpp
+++ source/vlc/cpp/src/LibVLC.cpp
@@ -1,8 +1,9 @@
 #include <mutex>
 #include <iostream>
 #include <string>
-#include <StdInt.h>
-#include <windows.h> 
+#include <stdint.h>
+// #include <StdInt.h>
+// #include <windows.h> 
 
 using std::string;
 using namespace std;
