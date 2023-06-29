0c8e912b8a5c5c1db08460d51f4521bc4cfc8486
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-06-29 05:47:48 +0300

    Shader Resize fix, thanks Ne_Eo

--- source/Main.hx
+++ source/Main.hx
@@ -13,28 +13,53 @@ class Main extends Sprite
 
 	public static var fpsCounter:FPS;
 
-	public function new()
+	public function new():Void
 	{
 		super();
 
-		FlxG.signals.preStateCreate.add(function(state:FlxState)
-		{
-			// Clear the loaded graphics if they are no longer in flixel cache...
-			for (key in Assets.cache.getBitmapKeys())
-				if (!FlxG.bitmap.checkCache(key))
-					Assets.cache.removeBitmapData(key);
-
-			// Clear the loaded songs as they use the most memory...
-			Assets.cache.clear('assets/songs');
-
-			// Run the garbage colector...
-			openfl.system.System.gc();
-		});
-		FlxG.signals.postStateSwitch.add(openfl.system.System.gc);
+		FlxG.signals.gameResized.add(onResizeGame);
+		FlxG.signals.preStateCreate.add(onStateCreate);
 
 		addChild(new FlxGame(1280, 720, Startup, 144, 144, true));
 
-		fpsCounter = new FPS();
+		fpsCounter = new FPS(10, 10, 0xFFFFFFFF);
 		addChild(fpsCounter);
+	}
+
+	private function onResizeGame(width:Int, heigth:Int):Void
+	{
+		if (FlxG.cameras == null)
+			return;
+
+		for (cam in FlxG.cameras.list)
+		{
+			@:privateAccess
+			if (cam != null && (cam._filters != null && cam._filters.length > 0))
+			{
+				var sprite:Sprite = cam.flashSprite; // Shout out to Ne_Eo for bringing this to my attention
+				if (sprite != null)
+				{
+					sprite.__cacheBitmap = null;
+					sprite.__cacheBitmapData = null;
+					sprite.__cacheBitmapData2 = null;
+					sprite.__cacheBitmapData3 = null;
+					sprite.__cacheBitmapColorTransform = null;
+				}
+			}
+		}
+	}
+
+	private function onStateCreate(state:FlxState):Void
+	{
+		// Clear the loaded graphics if they are no longer in flixel cache...
+		for (key in Assets.cache.getBitmapKeys())
+			if (!FlxG.bitmap.checkCache(key))
+				Assets.cache.removeBitmapData(key);
+
+		// Clear the loaded songs as they use the most memory...
+		Assets.cache.clear('assets/songs');
+
+		// Run the garbage colector...
+		openfl.system.System.gc();
 	}
 }
